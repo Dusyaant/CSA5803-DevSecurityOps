ad1607c72b46078ca2d9131ae753c84052350f42
Dusyaant R <dusyaant24@gmail.com>
2026-07-30 10:27:50 +0530

    Create Find the Biggest of Given Two Numbers.R

---
+++ Find the Biggest of Given Two Numbers.R
@@ -0,0 +1,12 @@
+# Define two numbers
+a <- 42
+b <- 87
+
+# Compare numbers
+if (a > b) {
+  print(paste("The biggest number is:", a))
+} else if (b > a) {
+  print(paste("The biggest number is:", b))
+} else {
+  print("Both numbers are equal")
+}
