89ae14114ad64de27a5a6619d945335719e65ee0
Dusyaant R <dusyaant24@gmail.com>
2026-07-30 10:30:37 +0530

    Add R script to calculate sum of two numbers

---
+++ Calculate the Sum of Two Numbers Using User Input.R
@@ -0,0 +1,7 @@
+# Prompt user for inputs
+num1 <- as.numeric(readline(prompt = "Enter first number: "))
+num2 <- as.numeric(readline(prompt = "Enter second number: "))
+
+# Calculate and display sum
+sum_input <- num1 + num2
+print(paste("The sum of entered numbers is:", sum_input))
