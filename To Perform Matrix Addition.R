d67af400a2dc3ec1a66931f2ae2ead895854c8b2
Dusyaant R <dusyaant24@gmail.com>
2026-07-30 10:38:16 +0530

    Create To Perform Matrix Addition.R

---
+++ To Perform Matrix Addition.R
@@ -0,0 +1,9 @@
+# Create two 2x2 matrices
+matrixA <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+matrixB <- matrix(c(5, 6, 7, 8), nrow = 2, ncol = 2)
+
+# Element-wise addition
+matrix_sum <- matrixA + matrixB
+
+print("Matrix Addition Result:")
+print(matrix_sum)
